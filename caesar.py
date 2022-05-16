41eaaaa16d753b6be3ba4914b420c5a872bcf3e9
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-05-16 01:03:07 -0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,6 +1,7 @@
 def encrypt(key,plaintext):
     
     print(plaintext)
+    print(key)
     ciphertext=""
 
     for char in plaintext:
@@ -11,10 +12,15 @@ def encrypt(key,plaintext):
     return ciphertext
 
 def decrypt(key,ciphertext):
+    
+    print(ciphertext)
+    print(key)
 
     plaintext=""
 
     for char in ciphertext:
         plaintext += chr((ord(char) - key - 65) % 26 + 65)
+        
+    print(plaintext)
 
     return plaintext
